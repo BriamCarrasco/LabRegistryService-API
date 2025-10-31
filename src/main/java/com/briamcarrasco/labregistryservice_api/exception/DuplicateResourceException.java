5034232695bfd01a26959395c511bdfa3ffade7a
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-10-31 15:30:09 -0300

    Se crea clase para mostrar mensaje personalizado para excepcion por registro duplicado

---
+++ src/main/java/com/briamcarrasco/labregistryservice_api/exception/DuplicateResourceException.java
@@ -0,0 +1,8 @@
+package com.briamcarrasco.labregistryservice_api.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+    
+}
