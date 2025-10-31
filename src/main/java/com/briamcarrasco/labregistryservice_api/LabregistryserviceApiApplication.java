7612f2fcfc4f4bff5f637fba3d99ecf2e3d298a6
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-10-31 04:00:29 -0300

    Archivos iniciales

---
+++ src/main/java/com/briamcarrasco/labregistryservice_api/LabregistryserviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.briamcarrasco.labregistryservice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabregistryserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabregistryserviceApiApplication.class, args);
+	}
+
+}
